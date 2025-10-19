3a8c73e373700583d9dca1a223db850c455c226d
500loop <phuongdongbinhlong@gmail.com>
2025-10-20 01:17:44 +0700

    chore

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,6 +9,7 @@ import orderRouter from './orderRoutes.js';
 import cartRouter from './cartRoutes.js';
 import contactRouter from './contactRoutes.js';
 import authRouter from './authRoutes.js';
+import favoritePostRouter from './favoritePostRoutes.js';
 const router = express.Router();
 
 router.use('/users', userRouter);
@@ -21,5 +22,6 @@ router.use('/orders', orderRouter);
 router.use('/carts', cartRouter);
 router.use('/contacts', contactRouter);
 router.use('/auth', authRouter);
+router.use('/favorites', favoritePostRouter);
 
 export default router;
