ee0e8fbc7939102d308b166b83bc9dcc97c0ef7f
Kietlatoi <toilakiet24@gmail.com>
2025-10-28 11:27:20 +0700

    change_from_Component_to_Link

--- frontend/src/features/admin/pages/AdminPage.jsx
+++ frontend/src/features/admin/pages/AdminPage.jsx
@@ -1,33 +1,11 @@
 import { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
-import ReportsStatistics from "./ReportsStatistics";
-import SystemFeesManagement from "./SystemFeesManagement";
-import TransactionManagement from "./TransactionManagement";
-import ListingManagement from "./ListingManagement";
-import UserManagement from "./UserManagement";
 import { getOrders } from "../service";
+import { Outlet } from "react-router-dom";
 
 const AdminPage = () => {
-  const [activeTab, setActiveTab] = useState("reports");
   const [isCollapsed, setIsCollapsed] = useState(false);
 
-  const renderContent = () => {
-    switch (activeTab) {
-      case "reports":
-        return <ReportsStatistics />;
-      case "fees":
-        return <SystemFeesManagement />;
-      case "transactions":
-        return <TransactionManagement />;
-      case "listings":
-        return <ListingManagement />;
-      case "users":
-        return <UserManagement />;
-
-      default:
-        return <ReportsStatistics />;
-    }
-  };
   useEffect(() => {
     const fetchOrders = async () => {
       try {
@@ -41,16 +19,13 @@ const AdminPage = () => {
   return (
     <div className="flex min-h-screen bg-gray-50">
       {/* Sidebar */}
-      <Sidebar
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-        isCollapsed={isCollapsed}
-        setIsCollapsed={setIsCollapsed}
-      />
+      <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
 
       {/* Main Content */}
       <div className="flex-1 overflow-auto">
-        <main className="h-full">{renderContent()}</main>
+        <main className="h-full">
+          <Outlet /> {/* Nơi route con được render */}
+        </main>
       </div>
     </div>
   );
