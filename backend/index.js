993f60bd9f99eefd4cf61126d4c35d7c70e7a360
vanchuong2805 <chuongv2805@gmail.com>
2025-10-29 14:16:23 +0700

    fix: httpOnly Cookies

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,18 @@ import router from './routes/index.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import './cronJobs/autoTransfer.js';
-import postService from './services/post/postService.js';
-import orderService from './services/order/orderService.js';
 
-postService.getCartItem(23);
 async function run() {
     dotenv.config();
     const PORT = process.env.PORT || 3000;
     const app = express();
     app.use(express.json());
-    app.use(cors());
+    app.use(cors(
+        {
+            origin: true,
+            credentials: true,
+        }
+    ));
     app.use(cookieParser());
     app.use('/api', router);
     app.listen(PORT, () => {
