0b3dcba1633da016fa3289985f545a21562144b8
tiana1314 <tuanhbinglongbp@gmail.com>
2025-11-13 00:54:31 +0700

    refactor(ui): update and refine FavoritesList interface layout

--- frontend/src/features/profile/components/favorites/FavoritesCard.jsx
+++ frontend/src/features/profile/components/favorites/FavoritesCard.jsx
@@ -10,30 +10,32 @@ export default function FavoriteCard({ listing, onView, onRemove }) {
 
 
   return (
-    <Frame
-      listing={listing}
-      tone="accent"
-      badgeText={listing.status==1 ? 'Đang bán' : 'Đã bán'}
-      note="Tin bạn đã quan tâm"
-      actions={[
-        <Button size="lg"
-          variant="outline"
-          className="h-10 w-full"
-          onClick={() =>
-            navigate(`/marketplace/listing/${listing.id}`, {
-              state: { from: `/profile/favorites` }
-            })
-          }>Chi tiết</Button>,
-        <Button
-          key="remove"
-          size="lg"
-          className="h-10 w-full bg-red-600 text-white hover:bg-red-700"
-          onClick={() => {toggleFavorite(listing.id) 
-            console.log('Bỏ quan tâm:', listing.id)}}
-        >
-          Bỏ quan tâm
-        </Button>,
-      ]}
-    />
+    <div onClick={() =>
+      navigate(`/marketplace/listing/${listing.id}`, {
+        state: { from: `/profile/favorites` }
+      })
+    }>
+
+      <Frame
+        listing={listing}
+        tone="accent"
+        badgeText={listing.status == 1 ? 'Đang bán' : 'Đã bán'}
+        note="Tin bạn đã quan tâm"
+        actions={[
+          <Button
+            key="remove"
+            size="lg"
+            className="h-10 w-full bg-red-600 text-white hover:bg-red-700"
+            onClick={(e) => {
+              e.stopPropagation()
+              toggleFavorite(listing.id)
+              console.log('Bỏ quan tâm:', listing.id)
+            }}
+          >
+            Bỏ quan tâm
+          </Button>,
+        ]}
+      />
+    </div>
   )
 }
