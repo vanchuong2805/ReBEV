accdb993469df5fd9c0501c9c5415080dedde9fa
tiana1314 <tuanhbinglongbp@gmail.com>
2025-11-21 06:34:53 +0700

    fix(sales): add status

--- frontend/src/features/profile/components/sales/SalesSection.jsx
+++ frontend/src/features/profile/components/sales/SalesSection.jsx
@@ -157,6 +157,7 @@ const SalesSection = () => {
                           ? "canceled"
                           : "refunded"
               }
+              status={status}
             />
           )
         })}
